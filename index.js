596b1b3128244e19320f7902e0a68c526029c055
dantemessy <thepunishar1@gmail.com>
2020-03-09 09:48:04 +0200

    ongodb address updated

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const server = require('./src/lib/server.js');
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://localhost:27017/please' ;
+const MONGODB_URI = 'mongodb://ds059634.mlab.com:59634/heroku_svj5r4zs' ;
+// const MONGODB_URI = 'mongodb://localhost:27017/database' ;
+
 const mongooseOption = {
   useNewUrlParser: true ,
   useCreateIndex: true ,
