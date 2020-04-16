dae86207ed10790b4e54ac61e3846391fcb8ff88
dantemessy <thepunishar1@gmail.com>
2020-04-16 17:26:56 +0300

    adding mlab DB

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const server = require('./src/lib/server.js');
 const mongoose = require('mongoose');
 
 // const MONGODB_URI = 'mongodb://admin:1994abdo@ds059634.mlab.com:59634/heroku_svj5r4zs' ;
-
 const MONGODB_URI = 'mongodb://ds059634.mlab.com:59634/heroku_svj5r4zs' ;
+
 // const MONGODB_URI = 'mongodb://localhost:27017/test121' ;
 
 const mongooseOption = {
